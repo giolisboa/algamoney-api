902f944d83ec4b864bdaf87123c2e1a226a6b6d6
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2022-01-13 20:56:29 -0300

    6.12. Adicionando permissões de acesso

--- src/main/java/com/algaworks/algamoney/api/resoure/CategoriaResource.java
+++ src/main/java/com/algaworks/algamoney/api/resoure/CategoriaResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,11 +33,13 @@ public class CategoriaResource {
     private ApplicationEventPublisher publisher;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and hasAuthority('SCOPE_read')")
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
     }
 
     @GetMapping("/{codigo}")
+    @PreAuthorize("hasAuthority('ROLE_PESQUISAR_CATEGORIA') and hasAuthority('SCOPE_read')")
     public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
         Optional<Categoria> categoria = this.categoriaRepository.findById(codigo);
 
@@ -46,6 +49,7 @@ public class CategoriaResource {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_CADASTRAR_CATEGORIA') and hasAuthority('SCOPE_write')")
     public ResponseEntity<Categoria> criar(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
         Categoria categoriaSalva = categoriaRepository.save(categoria);
         publisher.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
